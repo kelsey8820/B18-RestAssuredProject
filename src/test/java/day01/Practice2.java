08c2792fddf6bd24cd76eb0146f91f70f1f87198
Akbar <akbar@cybertekschool.com>
2020-08-18 15:53:43 -0400

    Hamcrest library collection support

--- src/test/java/day01/Practice2.java
+++ src/test/java/day01/Practice2.java
@@ -1,6 +1,10 @@
 package day01;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -61,6 +65,29 @@ public class Practice2 {
         assertThat(firstName , equalToIgnoringCase("kaisar ") );
         // how to ignore all whitespaces
         assertThat(firstName, equalToCompressingWhiteSpace("Kaisar"));
+
+
+    }
+
+    @DisplayName("Support for collection")
+    @Test
+    public void test2(){
+
+        List<Integer> numList = Arrays.asList(11,44,3,55,88,5) ;
+        // checking the list size is 6
+        assertThat(numList, hasSize(6)  );
+        // checking the list contains 11
+        assertThat(numList, hasItem(11) );
+
+        // has items is used to check multiple items
+        assertThat(numList, hasItems(11,3,5) );
+        //
+        // checking the list contains all the items in exact order : 11,44,3,55,88,5
+        assertThat(numList, contains(11,44,3,55,88,5 )   );
+
+        // checking the list contains all the items in any order  : 11, 44, 55,3,88
+        assertThat(numList,  containsInAnyOrder(11, 44, 55,3,88,5 )   );
+
 
 
     }
