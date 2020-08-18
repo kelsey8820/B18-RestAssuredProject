af93e38e0052594983f2f8953ef04cf407ec9e1a
Akbar <akbar@cybertekschool.com>
2020-08-18 16:04:18 -0400

    Method chaining start

---
+++ src/test/java/day01/RestAssuredMethodChaining.java
@@ -0,0 +1,26 @@
+package day01;
+
+import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.* ;
+import static io.restassured.matcher.RestAssuredMatchers.* ;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RestAssuredMethodChaining {
+
+    @DisplayName("First Attempt for chaining")
+    @Test
+    public void chainingMethodsTest1(){
+
+
+
+
+    }
+
+
+}
