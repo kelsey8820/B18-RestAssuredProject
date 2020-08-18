df2782803cbc537c76f018fed4130ffa8af9b2e8
Akbar <akbar@cybertekschool.com>
2020-08-18 12:28:21 -0400

    adding very first get method and saving response

--- src/test/java/day01/Practice1.java
+++ src/test/java/day01/Practice1.java
@@ -1,5 +1,8 @@
 package day01;
-
+import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.* ;
 import static io.restassured.matcher.RestAssuredMatchers.* ;
@@ -8,7 +11,26 @@ import static org.hamcrest.Matchers.*;
 
 public class Practice1 {
 
-    //http://52.71.242.164:8000/api/hello
+    // you may use your own IP
+    // we are using spartan app that does not require password
+    //http://54.174.216.245:8000/api/hello
+
+    @Test
+    public void test1(){
+
+        //RestAssured.get("URL HERE")
+        // SINCE WE DID THE STATIC IMPORT
+        // we can directly call the get method
+        // after we send the request
+        // we can save the result in to a type called Response
+        // need this  import io.restassured.response.Response;
+        Response response = get("http://54.174.216.245:8000/api/hello") ;
+
+
+    }
+
+
+
 
 
 
