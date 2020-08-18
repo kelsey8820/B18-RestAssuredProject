edb8153441e697f0dd2ac625c21cc044c29dcc4b
Akbar <akbar@cybertekschool.com>
2020-08-18 12:16:49 -0400

    added the first class and the static imports instructed by the document

---
+++ src/test/java/day01/Practice1.java
@@ -0,0 +1,15 @@
+package day01;
+
+
+import static io.restassured.RestAssured.* ;
+import static io.restassured.matcher.RestAssuredMatchers.* ;
+import static org.hamcrest.Matchers.*;
+
+
+public class Practice1 {
+
+    //http://52.71.242.164:8000/api/hello
+
+
+
+}
