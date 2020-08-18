02b1942d3583ca6480bcbd4b8f0d7d06a12a40b4
Akbar <akbar@cybertekschool.com>
2020-08-18 16:47:11 -0400

    Method chaining start -- given when then style in one shot

--- src/test/java/day01/RestAssuredMethodChaining.java
+++ src/test/java/day01/RestAssuredMethodChaining.java
@@ -17,7 +17,7 @@ public class RestAssuredMethodChaining {
     @Test
     public void chainingMethodsTest1(){
 
-//        given().
+//       given().
             // some more information you want to provide other than request url
             // like header , query param , path variable , body
 //       when()
@@ -41,12 +41,32 @@ public class RestAssuredMethodChaining {
 
         when().
                 get("http://54.174.216.245:8000/api/hello").   // sending a request to this url
-                then().                         // asssrtions start here
+                prettyPeek().
+        then().                         // asssrtions start here
                 statusCode( is(200) ).        // asserting status code is 200
-                header("Content-Length",  equalTo("17") ) ;  // asserting header is "17"
+                header("Content-Length",  equalTo("17") ).
+                header("Content-Type", is("text/plain;charset=UTF-8") ).
+                body( is("Hello from Sparta!") );
+                    ;  // asserting header is "17"
         ;
 
     }
+    @DisplayName("Testing GET /api/Spartans endpoint")
+    @Test
+    public void testAllSpartans(){
+
+
+            given().  // add all your request specific information like header, query param, path var, body
+                    header("Accept","application/xml").
+            when().
+                    get("http://54.174.216.245:8000/api/spartans").
+                    prettyPeek().
+            then().
+                    statusCode( is(200) ) ;
+
+    }
+
+
 
 
 
