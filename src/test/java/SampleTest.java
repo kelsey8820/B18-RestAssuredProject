cef17ceb812ee743b91c0cb824b0a61c6b05e841
Akbar <akbar@cybertekschool.com>
2020-08-18 11:12:03 -0400

    second test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,5 +1,6 @@
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,10 +18,25 @@ public class SampleTest {
 
         // how do we add error message if the assertion fail
 
-        assertEquals(10, 5+4 , "Hey wrong result!!");
+        //assertEquals(10, 5+4 , "Hey wrong result!!");
 
 
     }
+
+    // you can add the display name for your test instead of the method name
+    // providing the custom name for the test.
+    @DisplayName("I am testing the name")
+    @Test
+    public void nameTest(){
+
+        // write a simple assertion
+        // concatenate your first name and last name
+        // and make assertion it's equal to your full name
+
+    }
+
+
+
 
 
 }
