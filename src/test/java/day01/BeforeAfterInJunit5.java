b976d0c69c31e459e4a08676f84e1cfd38caca04
Akbar <akbar@cybertekschool.com>
2020-08-18 11:24:58 -0400

    BeforeAll , AfterAll ,BeforeEach, AfterEach

--- src/test/java/day01/BeforeAfterInJunit5.java
+++ src/test/java/day01/BeforeAfterInJunit5.java
@@ -1,6 +1,30 @@
 package day01;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class BeforeAfterInJunit5 {
+
+    // this method will run only once before the entire test
+    // same idea as @BeforeClass you learned previously
+    // these are JUNIT5 Specific annotations
+    @BeforeAll
+    public static void setUp(){
+        System.out.println("This run before All");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test is running");
+    }
+
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("This run all the way at the end");
+    }
+
 
 
 }
