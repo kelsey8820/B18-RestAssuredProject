51b27c143ea0266c9a77322b8a76f9ceb66b3ad5
Akbar <akbar@cybertekschool.com>
2020-08-18 16:19:01 -0400

    Method chaining start -- using hamcrest matcher to validate response

--- src/test/java/day01/RestAssuredMethodChaining.java
+++ src/test/java/day01/RestAssuredMethodChaining.java
@@ -17,10 +17,38 @@ public class RestAssuredMethodChaining {
     @Test
     public void chainingMethodsTest1(){
 
+//        given().
+            // some more information you want to provide other than request url
+            // like header , query param , path variable , body
+//       when()
+            // you send the request GET POST PUT PATCH DELETE
+//        then()
+            // VALIDATE SOMETHING HERE
 
+         //http://54.174.216.245:8000/api/hello
+        when().
+                get("http://54.174.216.245:8000/api/hello").   // sending a request to this url
+        then().                         // asssrtions start here
+                statusCode(200).        // asserting status code is 200
+                header("Content-Length", "17" ) ;  // asserting header is "17"
 
 
     }
+    @DisplayName("Using Hamcrest matcher for assertion")
+    @Test
+    public void testingWithMatcher(){
+
+
+        when().
+                get("http://54.174.216.245:8000/api/hello").   // sending a request to this url
+                then().                         // asssrtions start here
+                statusCode( is(200) ).        // asserting status code is 200
+                header("Content-Length",  equalTo("17") ) ;  // asserting header is "17"
+        ;
+
+    }
+
+
 
 
 }
