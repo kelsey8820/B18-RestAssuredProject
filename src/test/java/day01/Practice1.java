b279925984ff19cae161f50f1fcaba9286a52005
Akbar <akbar@cybertekschool.com>
2020-08-18 12:40:29 -0400

    getting information out of the response object separately

--- src/test/java/day01/Practice1.java
+++ src/test/java/day01/Practice1.java
@@ -18,6 +18,9 @@ public class Practice1 {
     @Test
     public void test1(){
 
+        // MAKE SURE YOUR REQUEST WORKS IN POSTMAN
+        // IF ANYTHING DOES NOT WORK MANNUALY IT WILL NOT WORK IN HERE EITHER
+
         //RestAssured.get("URL HERE")
         // SINCE WE DID THE STATIC IMPORT
         // we can directly call the get method
@@ -25,7 +28,20 @@ public class Practice1 {
         // we can save the result in to a type called Response
         // need this  import io.restassured.response.Response;
         Response response = get("http://54.174.216.245:8000/api/hello") ;
+        // response object store all the information about the response we got
+        // like status  , statusline , body , headers and so on
+        System.out.println("status code of this response : " + response.statusCode()  );
+        // this is another way of getting status code starts with HTTP/1.1
+        System.out.println("getting status line of this response " + response.statusLine() );
 
+        // in restAssured there are usually 2 methods that does same action
+        // one directly with the name like response.statusCode()
+        // another stating with getXXX  like response.getStatusCode()
+        System.out.println("status code of this response : " + response.getStatusCode()  );
+        // getting the header out of the response
+        // we can use       response.header("the header name goes here )
+        // or we can use    response.getHeader("the header name goes here )
+        System.out.println("Getting the value of date header " + response.header("Date") ) ;
 
     }
 
