c2c85f0c659de27baa48d9bb31f10be21c5168de
Akbar <akbar@cybertekschool.com>
2020-08-20 16:19:13 -0400

    Day02 : Query Parameters support with RestAssured

--- src/test/java/day02/SpartanSearchTest_QueryParam.java
+++ src/test/java/day02/SpartanSearchTest_QueryParam.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+
 public class SpartanSearchTest_QueryParam {
 
 
@@ -19,8 +20,25 @@ public class SpartanSearchTest_QueryParam {
         RestAssured.basePath = "/api" ;
 
     }
+    //http://54.174.216.245:8000/api/spartans/search?gender=male&nameContains=ea
+    @DisplayName("Testing /spartans/search Endpoint")
+    @Test
+    public void testSearch(){
+
+        given()
+                .log().all()
+                .queryParam("gender","male")
+                .queryParam("nameContains","a").
+
+        when()
+                .get("spartans/search").
+        then()
+                .log().all()
+                .statusCode(200)
+        ;
 
 
+    }
 
 
 }
