c3a9023ada1a24e9c73efdc2513075040ff7f159
Akbar <akbar@cybertekschool.com>
2020-08-20 11:28:40 -0400

    Day02 : RestAssured intro to BaseURI , BasePath

--- src/test/java/day02/SpartanTest.java
+++ src/test/java/day02/SpartanTest.java
@@ -1,6 +1,7 @@
 package day02;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,12 +23,40 @@ public class SpartanTest {
         // it will create the request URL as is
         // baseURI + basePath + what is after get( "This Part" )
 
+        // i want to explicitly specify I want JSON response from this result
+        given()
+                .header("Accept","application/json").
         when()
                 .get("/spartans").
         then()
                 .statusCode( is(200) ) ;
 
     }
+
+
+    @DisplayName("Get All Spartan 2 ")
+    @Test
+    public void testAllSpartans2(){
+
+
+        // send the same request specifiying the accept header is application/json
+        // use baseuri basepath , check status code 200 , contentType header is json
+
+        given()
+                .baseUri("http://54.174.216.245:8000")  // alternative way of doing it
+                .basePath("/api")
+                .accept("application/json").
+        when()
+                .get("/spartans").
+        then()
+                .statusCode( is(200) )
+                //.contentType("application/json;charset=UTF-8")
+                //.contentType( is("application/json;charset=UTF-8")  )
+                .contentType( ContentType.JSON )  ;
+
+
+    }
+
 
 
 
