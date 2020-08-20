caa61b74642f087da0e7c3013e79673a4d95e152
Akbar <akbar@cybertekschool.com>
2020-08-20 11:47:59 -0400

    Day02 : testing out hello endpoint with what we just learned

--- src/test/java/day02/SpartanTest2.java
+++ src/test/java/day02/SpartanTest2.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class SpartanTest2 {
@@ -19,13 +20,31 @@ public class SpartanTest2 {
         RestAssured.basePath = "/api" ;
 
     }
+    // quick tasks
+    // add another test for hello endpoint by reusing the baseURI , basePath above
+    // specify you want to get a text result back
+    // check status code is 200
+    // contentType is Text
+    // body is Hello from Sparta
+    @DisplayName("Testing /Hello again")
+    @Test
+    public void testHello(){
+
+        given()
+                .accept(ContentType.TEXT).
+        when()
+                .get("/hello").
+        then()
+                .statusCode( is(200) )
+                .contentType( ContentType.TEXT)
+                .body(equalTo("Hello from Sparta")) ;
+
+    }
 
 
     @DisplayName("Get All Spartan")
     @Test
     public void testAllSpartans(){
-
-
 
         // it will create the request URL as is
         // baseURI + basePath + what is after get( "This Part" )
@@ -50,8 +69,8 @@ public class SpartanTest2 {
         // use baseuri basepath , check status code 200 , contentType header is json
 
         given()
-                .baseUri("http://54.174.216.245:8000")  // alternative way of doing it
-                .basePath("/api")
+//                .baseUri("http://54.174.216.245:8000")  // alternative way of doing it
+//                .basePath("/api")
 //                .accept("application/json").
                 .accept(ContentType.JSON).
         when()
