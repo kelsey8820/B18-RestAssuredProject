66b42db71aa900c2b80ea9ad5fbe765342707bad
Akbar <akbar@cybertekschool.com>
2020-08-21 11:12:01 -0400

    Day03 : extracting the movie data out of response

--- src/test/java/day03/ExtractingDataOutOfResponseBody.java
+++ src/test/java/day03/ExtractingDataOutOfResponseBody.java
@@ -1,7 +1,9 @@
 package day03;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -12,6 +14,18 @@ public class ExtractingDataOutOfResponseBody {
     @Test
     public void test1(){
 
+        // provide your baseURI in the test
+        // add query parameters
+            // apikey yourAPIkey here
+            // t   = Boss Baby
+            // Save the response into response object
+
+        Response response = given()
+                .baseUri("http://www.omdbapi.com")
+                .queryParam("apikey","26aa5b74")
+                .queryParam("t","Boss Baby").
+        when()
+                .get();
 
 
     }
