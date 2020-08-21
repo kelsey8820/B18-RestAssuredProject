f24a55c60c7a9925c93e532a4c417c40b4795c6e
Akbar <akbar@cybertekschool.com>
2020-08-21 14:49:24 -0400

    Day03 : Testing jsonPath knowledge for search endpoint :getList method

--- src/test/java/day03/SpartanSearchExtractData.java
+++ src/test/java/day03/SpartanSearchExtractData.java
@@ -1,9 +1,12 @@
 package day03;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -22,9 +25,29 @@ public class SpartanSearchExtractData {
     public void test1(){
 
         Response response =  given()
+                                    .log().all()    // just to see my request in case anything goes wrong
                                     .queryParam("gender","Female").
                              when()
-                                     .get("/spartans/search") ;
+                                     .get("/spartans/search")
+                                     .prettyPeek()
+                                        ;
+
+        JsonPath jp = response.jsonPath();
+        // get the value of numberOfElements from the response body
+        int numOfFemaleSpartans  =  jp.getInt("numberOfElements");
+        System.out.println("numOfFemaleSpartans = " + numOfFemaleSpartans);
+
+        // if you wanted to get single Spartan , for example the first one id
+        // you would use jsonPath of  content[0].id
+        // if you want to get al the ids , You can use getList method and remove the index
+            //  content.id  for the id , content.name
+
+        // storing all ids into list of integer
+        List<Integer> numList = jp.getList("content.id");
+        System.out.println("numList = " + numList);
+        List<String> nameList = jp.getList("content.name");
+        System.out.println("nameList = " + nameList);
+
 
     }
 
