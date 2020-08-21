139c13e811f041890d28f4a863bb9145f042b274
Akbar <akbar@cybertekschool.com>
2020-08-21 11:35:04 -0400

    Day03 : extracting the movie data out of response using path method of Response Object

--- src/test/java/day03/ExtractingDataOutOfResponseBody.java
+++ src/test/java/day03/ExtractingDataOutOfResponseBody.java
@@ -31,11 +31,24 @@ public class ExtractingDataOutOfResponseBody {
 
      //response.prettyPrint() ;
      System.out.println(  response.statusCode()  );
-
      // if you want to get single data out for example just title , just year
         // use path method of Response object and provide the jsonPath
         String title = response.path("Title") ;
         System.out.println("title = " + title);
+        // using same example - print out -- Year, Director , Actors
+        // Optionally -- first Rating Source
+        String year = response.path("Year") ;
+        String director = response.path("Director") ;
+        String actors = response.path("Actors");
+
+        System.out.println("Year = " + year);
+        System.out.println("director = " + director);
+        System.out.println("actors = " + actors);
+
+        // getting the first Rating source
+        // the jsonPath for first Rating source is Ratings[0].Source
+        String firstRatingSrc = response.path("Ratings[0].Source");
+        System.out.println("firstRatingSrc = " + firstRatingSrc);
 
 
 
