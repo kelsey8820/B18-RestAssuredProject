4ae8fed789ede2d4f82f2ccdf9a9e93b6a3049ae
Akbar <akbar@cybertekschool.com>
2020-08-21 11:06:23 -0400

    Day03 : extracting the movie data out of response

---
+++ src/test/java/day03/ExtractingDataOutOfResponseBody.java
@@ -0,0 +1,20 @@
+package day03;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ExtractingDataOutOfResponseBody {
+
+    @DisplayName("Getting Movie info")
+    @Test
+    public void test1(){
+
+
+
+    }
+
+
+}
