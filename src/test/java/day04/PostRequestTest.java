605153d248e6557b662b4fcef35ed9d84840a0e4
Akbar <akbar@cybertekschool.com>
2020-08-24 14:08:46 -0400

    Day04 : Post Request Starter

---
+++ src/test/java/day04/PostRequestTest.java
@@ -0,0 +1,28 @@
+package day04;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class PostRequestTest {
+
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI = "http://54.158.178.13" ;
+        RestAssured.port = 8000 ;
+        RestAssured.basePath = "/api" ;
+
+    }
+
+
+
+}
