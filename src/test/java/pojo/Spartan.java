f84a9ea190c2aecd50ff2b425661ae3400e30536
Akbar <akbar@cybertekschool.com>
2020-08-24 15:43:19 -0400

    Day04 : Spartan Pojo

---
+++ src/test/java/pojo/Spartan.java
@@ -0,0 +1,41 @@
+package pojo;
+
+public class Spartan {
+
+    private String name;
+    private String gender;
+    private long phone;
+
+    public Spartan() {
+    }
+
+    public Spartan(String name, String gender, long phone) {
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}
