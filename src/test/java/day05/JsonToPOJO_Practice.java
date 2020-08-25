cd6ea440df2504edd912ab063154c0d86fee2318
Akbar <akbar@cybertekschool.com>
2020-08-25 12:56:02 -0400

    Day05 : Saving the Spartan Json into Java Object -- Spartan2 Pojo as type

--- src/test/java/day05/JsonToPOJO_Practice.java
+++ src/test/java/day05/JsonToPOJO_Practice.java
@@ -24,11 +24,12 @@ public class JsonToPOJO_Practice {
     @Test
     public void testSpartanJsonToSpartanObject(){
 
+        int newID = SecureSpartanTest.createRandomSpartan();
 
         Response response = given()
                                 .auth().basic("admin","admin")
                                 .log().all()
-                                .pathParam("id", 261).
+                                .pathParam("id", newID).
                         when()
                                 .get("/spartans/{id}")
                                 .prettyPeek() ;
@@ -36,12 +37,36 @@ public class JsonToPOJO_Practice {
         // accept a class type to define what is the type object you are trying to store the response as
         //  Spartan2 class we created has all the fields that match the json fields from the response
         // So it will map all the json fields to the java fields and return Spartan2 POJO Object
+        // in a simple word turn below json into Java object
+        /**
+         * {
+         *     "id": 261,
+         *     "name": "Elma",
+         *     "gender": "Male",
+         *     "phone": 9999999998
+         * }
+         *
+         * into  new Spartan2(261,"Elma","Male",9999999998L) Java Object
+         * so we can work with the data using java object directly
+         */
         Spartan2 sp = response.as( Spartan2.class ) ;
         // above line is almost as if you are doing below line
 //        Spartan2 sp = new Spartan2(261,"Elma","Male",9999999998L)
         System.out.println("sp = " + sp);
 
     }
+
+    @DisplayName("Search Request and save 1st Result as Spartan2 Pojo")
+    @Test
+    public void gettingNestedJsonAsPojo(){
+
+
+
+
+    }
+
+
+
 
 
 
