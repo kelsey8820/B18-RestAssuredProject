d352e5c2809f788d2b57728daf17e02c0a37fb08
Akbar <akbar@cybertekschool.com>
2020-08-25 11:30:24 -0400

    Day05 : Testing unauthorized request that return 401

--- src/test/java/day05/SecureSpartanTest.java
+++ src/test/java/day05/SecureSpartanTest.java
@@ -2,12 +2,21 @@ package day05;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
 
 public class SecureSpartanTest {
 
-     //54.160.106.84
+    // These instances are created from the image : Cybertek_Latest_BasicAuth
+    //54.160.106.84
     //100.24.235.129
     //23.23.75.140
+    // so it require username password to access the application
+    // yours is created from Cybertek_Latest_NoAuth
+    // it has the Spartan App version that does not require username password
+
      // add @BeforeAll and use the spartanApp ID with basic auth
     @BeforeAll
     public static void init(){
@@ -19,6 +28,22 @@ public class SecureSpartanTest {
 
      // add a test
      // make a simple get request /spartans/{id}
+    @DisplayName("Test GET /spartan/{id} Endpoint with No Credentials")
+    @Test
+    public void testGetSingleSpartanSecured(){
+
+        given()
+                .log().all()
+                .pathParam("id",174).
+        when()
+                .get("/spartans/{id}").
+        then()
+                .log().all()
+                .statusCode( 401 )
+        ;
+
+
+    }
 
 
 
