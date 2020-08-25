9dfcaeb78cada10c5cb98ef548e5c4eaef08158e
Akbar <akbar@cybertekschool.com>
2020-08-25 12:44:17 -0400

    Day05 : Saving the Spartan Json into Java Object -- Spartan2 Pojo as type

--- src/test/java/day05/JsonToPOJO_Practice.java
+++ src/test/java/day05/JsonToPOJO_Practice.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.Spartan;
+import pojo.Spartan2;
 
 import static io.restassured.RestAssured.*;
 
@@ -30,6 +32,14 @@ public class JsonToPOJO_Practice {
                         when()
                                 .get("/spartans/{id}")
                                 .prettyPeek() ;
+        // as method from response
+        // accept a class type to define what is the type object you are trying to store the response as
+        //  Spartan2 class we created has all the fields that match the json fields from the response
+        // So it will map all the json fields to the java fields and return Spartan2 POJO Object
+        Spartan2 sp = response.as( Spartan2.class ) ;
+        // above line is almost as if you are doing below line
+//        Spartan2 sp = new Spartan2(261,"Elma","Male",9999999998L)
+        System.out.println("sp = " + sp);
 
     }
 
