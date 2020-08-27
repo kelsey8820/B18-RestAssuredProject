20686cf878dbb6fea670c52a817413c4a9e4f75b
Akbar <akbar@cybertekschool.com>
2020-08-27 10:35:15 -0400

    Day06 : Location POJO

--- src/test/java/pojo/Locations.java
+++ src/test/java/pojo/Locations.java
@@ -1,5 +1,72 @@
 package pojo;
 
-//TODO Country POJO class that have same encapsulated field as Countries table
+
 public class Locations {
+
+    private int location_id ;
+    private String street_address ;
+    private String postal_code ;
+    private String city ;
+    private String state_province ;
+    private int country_id ;
+
+    @Override
+    public String toString() {
+        return "Locations{" +
+                "location_id=" + location_id +
+                ", street_address='" + street_address + '\'' +
+                ", postal_code='" + postal_code + '\'' +
+                ", city='" + city + '\'' +
+                ", state_province='" + state_province + '\'' +
+                ", country_id=" + country_id +
+                '}';
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(int location_id) {
+        this.location_id = location_id;
+    }
+
+    public String getStreet_address() {
+        return street_address;
+    }
+
+    public void setStreet_address(String street_address) {
+        this.street_address = street_address;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState_province() {
+        return state_province;
+    }
+
+    public void setState_province(String state_province) {
+        this.state_province = state_province;
+    }
+
+    public int getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(int country_id) {
+        this.country_id = country_id;
+    }
 }
