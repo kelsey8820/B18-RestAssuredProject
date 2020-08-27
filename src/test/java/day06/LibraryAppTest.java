9e9f7bb5fe8b806eba7568480c84f935891c3748
Akbar <akbar@cybertekschool.com>
2020-08-27 12:13:11 -0400

    Day06 : Practicing the POJO to JSON

--- src/test/java/day06/LibraryAppTest.java
+++ src/test/java/day06/LibraryAppTest.java
@@ -1,6 +1,8 @@
 package day06;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,7 @@ import static io.restassured.RestAssured.given;
 
 public class LibraryAppTest {
 
+    private static String libraryToken;
 
     @BeforeAll
     public static void init(){
@@ -31,5 +34,35 @@ public class LibraryAppTest {
                 .statusCode(200);
 
     }
+
+
+
+
+    /**
+     * A static utility method to get the token by providing username and password
+     * as Post request to /Login endpoint and capture the token field from response json
+     * @param username
+     * @param password
+     * @return the token as String from the response json
+     */
+    public static String loginAndGetToken(String username, String password){
+
+        String token = "";
+
+        Response response = given()
+//                                .log().all()
+                // explicitly saying the body content type is x-www-urlencoded-form-data
+                .contentType(ContentType.URLENC)
+                .formParam("email",username)
+                .formParam("password", password ).
+                        when()
+                .post("/login") ;
+
+        //token = response.path("token") ;  // this is using path method
+        token = response.jsonPath().getString("token") ;
+        return token ;
+    }
+
+
 
 }
