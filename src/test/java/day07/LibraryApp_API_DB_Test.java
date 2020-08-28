15926f14d6270c28aca5ca8843b5bab27a09d915
Akbar <akbar@cybertekschool.com>
2020-08-28 15:22:01 -0400

    Day07 : Using DB_Utility to run query Select count(*) from book and save the result

--- src/test/java/day07/LibraryApp_API_DB_Test.java
+++ src/test/java/day07/LibraryApp_API_DB_Test.java
@@ -3,10 +3,13 @@ package day07;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utility.ConfigurationReader;
 import utility.DB_Utility;
 import utility.LibraryUtil;
+
+import java.time.LocalDate;
 
 public class LibraryApp_API_DB_Test {
 
@@ -23,11 +26,16 @@ public class LibraryApp_API_DB_Test {
         DB_Utility.createConnection("library1");
 
     }
-
+    @DisplayName("Validating the /dashboard_stats endpoint data against Database")
     @Test
     public void test(){
         System.out.println("libraryToken = " + libraryToken);
         // We will make a call to /Dashboard_stats endpoint and validate the data against database data
+        DB_Utility.runQuery("SELECT count(*) from books"); // it return the book count as single row and col
+        String bookCount  =  DB_Utility.getColumnDataAtRow(1,1);
+
+
+
     }
 
 
