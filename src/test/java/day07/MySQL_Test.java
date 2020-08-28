e3243ae2ea366695a602eaa70f68a9874715d43b
Akbar <akbar@cybertekschool.com>
2020-08-28 14:17:34 -0400

    Day07 : Added test to test out connection with our query

--- src/test/java/day07/MySQL_Test.java
+++ src/test/java/day07/MySQL_Test.java
@@ -2,6 +2,8 @@ package day07;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import utility.DB_Utility;
 
 public class MySQL_Test {
@@ -18,6 +20,14 @@ public class MySQL_Test {
         DB_Utility.createConnection("library1");
     }
 
+    @DisplayName("Quick DB Check")
+    @Test
+    public void testThingsOut(){
+
+        DB_Utility.runQuery("SELECT * FROM books") ;
+        DB_Utility.displayAllData();
+
+    }
 
     @AfterAll
     public static void destroy(){
