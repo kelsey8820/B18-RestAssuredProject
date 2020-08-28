da92d1b7affff938a0388d024acaa6df7ab79f50
Akbar <akbar@cybertekschool.com>
2020-08-28 10:52:20 -0400

    Day06 :News API ,get all article author if source id is not null

--- src/test/java/day07/NewsAPI_Test.java
+++ src/test/java/day07/NewsAPI_Test.java
@@ -1,8 +1,11 @@
 package day07 ;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -25,8 +28,22 @@ public class NewsAPI_Test {
                                     .log().all().
                             when().
                                     get("/top-headlines");
+        JsonPath jp = response.jsonPath() ;
 
+        List<String> allAuthor =  jp.getList("articles.author") ;
+        System.out.println("allAuthor = " + allAuthor.size() );
+       // allAuthor.forEach( eachAuthor -> System.out.println("eachAuthor = " + eachAuthor));
+        // filter out the result by checking the source fields --->> id filed null or not
 
+        List<String> allAuthorFiltered
+                =  jp.getList("articles.findAll { it.source.id !=null }.author") ;
+
+        for (String author : allAuthorFiltered){
+
+            System.out.println("author = " + author);
+
+        }
+        System.out.println("allAuthorFiltered = " + allAuthorFiltered.size() );
 
 
     }
