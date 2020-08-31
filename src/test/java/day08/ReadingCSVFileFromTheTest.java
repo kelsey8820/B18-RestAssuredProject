ca2f92368b826f69737d333b8a2fe0c44fa47faf
Akbar <akbar@cybertekschool.com>
2020-08-31 15:26:24 -0400

    Day08 : Practicing ParameterizedTest DisplayName

--- src/test/java/day08/ReadingCSVFileFromTheTest.java
+++ src/test/java/day08/ReadingCSVFileFromTheTest.java
@@ -44,11 +44,22 @@ public class ReadingCSVFileFromTheTest {
     // What if I want some custom name
         // @ParameterizedTest(name = "Some custom name here")
     // How do I refer the row number in my csv file
-        // you can refer row number using {index} in the name String
+        // you can refer row number using exact text -->>{index} in the name String
     // How do I refer the column data in my display name
-    // WHERE DO I ADD the display name to start with ?
+        // in the name String we can add {yourColumnIndexHere}
+        // {0} for first column
+        // {1} for second column
+        // {2} for third column
 
-    @ParameterizedTest(name = "B18 Learning Math {index} ")
+    // WHERE DO I ADD the display name to start with ? add it to @ParameterizedTest (name = " here")
+
+    // we want display like this so we know exactly what we tested in each iteration
+    // Row1 : 5+4=9
+    // Row2 : 4+7=1
+
+
+    //@ParameterizedTest(name = "CURRENT ITERATION {index}  | ThirdCol {2} | FirstCol {0} | SecondCol {1} |  ")
+    @ParameterizedTest(name = "iteration {index} -> {0} + {1} = {2}")
     @CsvFileSource(resources = "/numbers.csv", numLinesToSkip = 1)
     public void testAddition2(int n1, int n2, int result){
         //import static org.junit.jupiter.api.Assertions.assertEquals;
