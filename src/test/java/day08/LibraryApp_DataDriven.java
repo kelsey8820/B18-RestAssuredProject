580658ef24818f7792ae21578d6ea040aa7ecab1
Akbar <akbar@cybertekschool.com>
2020-08-31 15:57:19 -0400

    Day08 : Data Driven Test for our POST /login Request for LibraryApp

--- src/test/java/day08/LibraryApp_DataDriven.java
+++ src/test/java/day08/LibraryApp_DataDriven.java
@@ -1,11 +1,15 @@
 package day08;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import utility.ConfigurationReader;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class LibraryApp_DataDriven {
 
@@ -23,9 +27,24 @@ public class LibraryApp_DataDriven {
     @CsvFileSource(resources = "/credentials.csv")
     public void testLoginCredentials(String user, String pass){
 
-        System.out.println("user = " + user);
-        System.out.println("pass = " + pass);
-
+//        System.out.println("user = " + user);
+//        System.out.println("pass = " + pass);
+          // so now lets make a post request to /login
+         // content type is x-www-form-urlencoded
+         // form data  email , password
+         // check if the status code 200  if the password is correct
+         // check the token field from the response is not null
+         given()
+                 .log().all()
+                .contentType(ContentType.URLENC)
+                .formParam("email", user )
+                 .formParam("password" , pass).
+         when()
+                 .post("/login").
+         then()
+                 .statusCode(200)
+                 .body("token", notNullValue() )
+                ;
 
     }
 
