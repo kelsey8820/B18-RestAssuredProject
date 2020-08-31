1ae17809ab1822313b9c3b439469c90a9725191f
Akbar <akbar@cybertekschool.com>
2020-08-31 12:38:05 -0400

    Day08 : Specifying the test execution order in junit 5

--- src/test/java/day08/TestExecutionOrderInJunit5.java
+++ src/test/java/day08/TestExecutionOrderInJunit5.java
@@ -1,20 +1,32 @@
 package day08;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+// Default test execution order is alphabetical
+// if you want to change it , this is one way to do it
+// This annotation indicate to run the tests according to the @Order annotation with number
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestExecutionOrderInJunit5 {
 
+    @Order(4)
+    @Test
+    public void testB(){
 
+    }
+    @Order(2)
     @Test
     public void testA(){
 
     }
-
+    @Order(3)
     @Test
     public void testZ(){
 
     }
-
+    @Order(1)
     @Test
     public void testK(){
 
