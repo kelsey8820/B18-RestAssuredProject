5336b0cc62b08ab5bf8dda8a8c0088670780eea9
Akbar <akbar@cybertekschool.com>
2020-09-01 14:31:48 -0400

    Day09 : Added ResponseSpecification object using ResponseSpecBuilder method chaining

--- src/test/java/day09/SpartanRoleBaseAccessControlNegativeTest_Reuse.java
+++ src/test/java/day09/SpartanRoleBaseAccessControlNegativeTest_Reuse.java
@@ -2,16 +2,17 @@ package day09;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pojo.Spartan;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class SpartanRoleBaseAccessControlNegativeTest_Reuse {
 
@@ -35,15 +36,21 @@ public class SpartanRoleBaseAccessControlNegativeTest_Reuse {
                 .accept(ContentType.JSON) // we are getting 403 with json body so accept header is json
                 .log().all();
 
+        // Extracting ResponseSpecification for all assertions so we can reuse
+        // We will be using a class called ResponseSpecBuilder
+        ResponseSpecBuilder resSpecBuilder = new ResponseSpecBuilder() ;
+        // Getting the reusable ResponseSpecification object using the builder methods chaining
+        ResponseSpecification responseSpec = resSpecBuilder.expectStatusCode(403)
+                                              .expectContentType(ContentType.JSON)
+                                              .expectHeader("Date",  not(null) )
+                                              .build();
+
         given()
                 .spec(requestSpec).
         when()
                 .delete("/spartans/{id}" , 10).
         then()
-                .statusCode(403)
-                .contentType(ContentType.JSON)
-                .header("Date", is( notNullValue() ) ) // checking Date header is not null
-                .log().all();
+                .spec(responseSpec);
 
     }
 
