5eac4e4de04685d8b58121f22d82ae3610ae0a4c
Akbar <akbar@cybertekschool.com>
2020-09-01 15:41:53 -0400

    Day09 : Extracting the post request request spec and response spec starter

---
+++ src/test/java/day09/SpartanPostRequestExtractingSpecTest.java
@@ -0,0 +1,35 @@
+package day09;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.Spartan;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanPostRequestExtractingSpecTest {
+
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "http://54.160.106.84";
+        RestAssured.port = 8000;
+        RestAssured.basePath = "/api";
+    }
+
+    @DisplayName("Extracting the requestSpec and responseSpec practice")
+    @Test
+    public void test(){
+
+    }
+
+
+
+
+
+}
