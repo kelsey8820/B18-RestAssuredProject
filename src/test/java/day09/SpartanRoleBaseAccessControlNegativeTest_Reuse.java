b2451f262cf0b326b7efb024301ddf4645e0bfd3
Akbar <akbar@cybertekschool.com>
2020-09-01 14:20:17 -0400

    Day09 : Extracted our first request specification out of the method chain.

---
+++ src/test/java/day09/SpartanRoleBaseAccessControlNegativeTest_Reuse.java
@@ -0,0 +1,98 @@
+package day09;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.Spartan;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class SpartanRoleBaseAccessControlNegativeTest_Reuse {
+
+
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI = "http://54.160.106.84";
+        RestAssured.port = 8000;
+        RestAssured.basePath = "/api";
+    }
+
+    @DisplayName("User should not be able to delete data")
+    @Test
+    public void testUserCanNotDeleteData(){
+
+        // building reusable request specification
+        // using RequestSpecBuilder class
+
+        RequestSpecification requestSpec = given()
+                .auth().basic("user", "user")
+                .accept(ContentType.JSON) // we are getting 403 with json body so accept header is json
+                .log().all();
+
+        given()
+                .spec(requestSpec).
+        when()
+                .delete("/spartans/{id}" , 10).
+        then()
+                .statusCode(403)
+                .contentType(ContentType.JSON)
+                .header("Date", is( notNullValue() ) ) // checking Date header is not null
+                .log().all();
+
+    }
+
+    @DisplayName("User should not be able to update data")
+    @Test
+    public void testUserCanNotUpdateData(){
+
+        Spartan spartanObj = new Spartan("some name", "Male", 8888888888L) ;
+
+        given()
+                .auth().basic("user","user")
+                .accept(ContentType.JSON)
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(spartanObj).
+        when()
+                .put("/spartans/{id}", 10).
+        then()
+                .statusCode(403)
+                .contentType(ContentType.JSON)
+                .header("Date", is( notNullValue() ) ) // checking Date header is not null
+                .log().all();
+
+
+    }
+
+    @DisplayName("User should not be able to post data")
+    @Test
+    public void testUserCanNotPostData(){
+
+        Spartan spartanObj = new Spartan("some name", "Male", 8888888888L) ;
+
+        given()
+                .auth().basic("user","user")
+                .accept(ContentType.JSON)
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(spartanObj).
+        when()
+                .post("/spartans").
+        then()
+                .statusCode(403)
+                .contentType(ContentType.JSON)
+                .header("Date", is( notNullValue() ) ) // checking Date header is not null
+                .log().all();
+
+    }
+
+
+
+
+}
