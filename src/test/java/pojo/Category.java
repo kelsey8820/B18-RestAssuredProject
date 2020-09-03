d186608f25a6b2db785a088e2e4acc0b892aa85a
Akbar <akbar@cybertekschool.com>
2020-09-03 12:52:24 -0400

    Day10 : Added the no boiler plate POJO

---
+++ src/test/java/pojo/Category.java
@@ -0,0 +1,15 @@
+package pojo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Category {
+
+    private String id;
+    private String name;
+
+}
