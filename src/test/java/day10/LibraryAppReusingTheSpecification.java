8fdcada0136107c203e2dd9f68f8a635fa1fffb3
Akbar <akbar@cybertekschool.com>
2020-09-03 10:45:52 -0400

    Day10 : Reviewing the Re-usable request and response spec

---
+++ src/test/java/day10/LibraryAppReusingTheSpecification.java
@@ -0,0 +1,53 @@
+package day10 ;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+import utility.LibraryUtil;
+
+import static io.restassured.RestAssured.*;
+
+public class LibraryAppReusingTheSpecification {
+
+    static RequestSpecification requestSpec ;
+    static ResponseSpecification responseSpec ;
+
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI = ConfigurationReader.getProperty("library1.base_url");
+        RestAssured.basePath = "/rest/v1" ;
+
+        String theToken = LibraryUtil.loginAndGetToken("librarian69@library","KNPXrm3S");
+        // we have build a Reusable request Specification for setting contentType
+        requestSpec = given().accept(ContentType.JSON)  // we want json back
+                         .log().all()               // we want to log all
+                         .header("x-library-token", theToken) ; // we want to set the token header
+        // is there easy way to directly create ResponseSpec object without the builder , YES THERE IS !!!!
+        responseSpec =  expect().statusCode(200)       // expecting the Response status code 200
+                                .contentType(ContentType.JSON)  // contentype is josn
+                                .logDetail(LogDetail.ALL) ;     // want to log all of them
+
+    }
+
+    @DisplayName("Testing GET /get_book_categories Endpoint with spec")
+    @Test
+    public void testLibrary(){
+
+            given()
+                    .spec(requestSpec).
+            when()
+                    .get("/get_book_categories").
+            then()
+                    .spec(responseSpec) ;
+
+    }
+
+
+}
