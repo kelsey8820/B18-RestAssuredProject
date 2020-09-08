dae4b7977ab3ffb464816d7bf11173b709bff36e
Akbar <akbar@cybertekschool.com>
2020-09-08 15:14:41 -0400

    Day 12 : /Spartans xml response extracting data using XMLPath object

--- src/test/java/day12/SpartanXML_Test.java
+++ src/test/java/day12/SpartanXML_Test.java
@@ -2,6 +2,8 @@ package day12;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,5 +39,29 @@ public class SpartanXML_Test {
 
 
     }
+    @DisplayName("XMLPath object to extract the data out")
+    @Test
+    public void testSpartanExtractData(){
+
+        Response response = given()
+                .accept(ContentType.XML).
+                        when()
+                .get("/spartans") ;
+        // getting XMLPath object just like we did for JsonPath object
+        XmlPath xp = response.xmlPath();
+
+        // get the first spartan id and store it into the int
+        int firstID = xp.getInt("List.item[0].id");
+        System.out.println("firstID = " + firstID);
+        String firstName = xp.getString("List.item[0].name") ;
+        System.out.println("firstName = " + firstName);
+        long firstPhone = xp.getLong("List.item[0].phone");
+        System.out.println("firstPhone = " + firstPhone);
+
+
+
+    }
+
+
 
 }
