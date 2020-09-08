1d35b9a31a0fc51765b22c6a1170d76b48c7fb4c
Akbar <akbar@cybertekschool.com>
2020-09-08 16:13:41 -0400

    Day 12 : Hamcrest matcher collection practice

---
+++ src/test/java/day12/HamcrestMatcherEveryItem.java
@@ -0,0 +1,18 @@
+package day12;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HamcrestMatcherEveryItem {
+
+    @Test
+    public void testList(){
+
+        List<Integer> nums = Arrays.asList(1,4,5,6,8,9,0) ;
+
+    }
+
+
+}
