6ae3a23ac1b398bb87cb0413b5ef1d67a62dc8b8
Akbar <akbar@cybertekschool.com>
2020-09-08 14:31:58 -0400

    Day 12 : /Spartans xml response practice starter

---
+++ src/test/java/day12/SpartanXML_Test.java
@@ -0,0 +1,18 @@
+package day12;
+
+import org.junit.jupiter.api.Test;
+
+public class SpartanXML_Test {
+
+    // boilerplate goes here
+
+
+
+    @Test
+    public void testSpartanXML(){
+
+
+
+    }
+
+}
