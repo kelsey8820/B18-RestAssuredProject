fadebab117f606e6b53f29cb70732e55e1b8dbfc
Akbar <akbar@cybertekschool.com>
2020-09-08 14:35:13 -0400

    Day 12 : /Spartans xml response practice added @Before all and imports

--- src/test/java/day12/SpartanXML_Test.java
+++ src/test/java/day12/SpartanXML_Test.java
@@ -1,13 +1,23 @@
 package day12;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utility.ConfigurationReader;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class SpartanXML_Test {
 
     // boilerplate goes here
+    @BeforeAll
+    public static void setUp(){
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartan1.base_url");
+        RestAssured.basePath = "/api";
+    }
 
-
-
+    @DisplayName("Test XML Response from GET /Spartans")
     @Test
     public void testSpartanXML(){
 
