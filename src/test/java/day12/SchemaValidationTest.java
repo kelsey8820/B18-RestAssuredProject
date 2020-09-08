7e606592c86ca5829e63bac64c193fdacb85443f
Akbar <akbar@cybertekschool.com>
2020-09-08 11:16:15 -0400

    Day 12 : Added the test for reading the schema file and providing it to matchesSchema method

--- src/test/java/day12/SchemaValidationTest.java
+++ src/test/java/day12/SchemaValidationTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utility.ConfigurationReader;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
@@ -30,6 +32,21 @@ public class SchemaValidationTest {
 
     }
 
+    @DisplayName("Testing GET /Spartans response against Schema in rootPath")
+    @Test
+    public void testAllSpartansSchemaInRootPath(){
+
+        // Create a File object that point to the schema
+        // use matchesJsonSchema method that accept a file
+        // and do your validation
+        File mySchema = new File("AllSpartans2Schema.json");
+        when()
+                .get("/spartans").
+                then()
+                .body( matchesJsonSchema(mySchema) ) ;
+
+    }
+
     @DisplayName("Testing GET /Spartans/search response against Schema")
     @Test
     public void testSearchSpartansSchema(){
@@ -43,5 +60,9 @@ public class SchemaValidationTest {
                 .body(matchesJsonSchemaInClasspath("SearchSchema.json") ) ;
 
     }
+    // what if my schema file is not under resources folder ?
+    // then matchesJsonSchemaInClasspath will not work because it only look for
+    // schema under resources folder
+    // then we have to use matchesJsonSchema method and provide full path
 
 }
