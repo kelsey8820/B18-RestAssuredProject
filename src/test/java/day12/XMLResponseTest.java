22760794970d9df1bf9b380d69c318c842635e95
Akbar <akbar@cybertekschool.com>
2020-09-08 11:50:39 -0400

    Day 12 : Trying the first XMLPath to find the count field

--- src/test/java/day12/XMLResponseTest.java
+++ src/test/java/day12/XMLResponseTest.java
@@ -1,16 +1,41 @@
 package day12;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class XMLResponseTest {
 
     @BeforeAll
     public static void init(){
         // We are going to send a get request to this endpoint
+        // in this Rest API Request , it use query parameter to specify the response content type
         //https://vpic.nhtsa.dot.gov/api/vehicles/GetMakeForManufacturer/988?format=xml
         RestAssured.baseURI = "https://vpic.nhtsa.dot.gov";
         RestAssured.basePath = "api/vehicles";
+    }
+
+    @Test
+    public void testXML(){
+
+        given()
+                .log().all()
+                .queryParam("format", "xml").
+        when()
+                .get("/GetMakeForManufacturer/988").
+        then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.XML)
+                // the path must match , and the value is always String in xml
+                .body("Response.Count" , is("2") )
+
+                ;
+
     }
 
 
