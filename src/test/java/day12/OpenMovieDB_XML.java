13d57df6719c703f7eabcd95a90b661d656a7cba
Akbar <akbar@cybertekschool.com>
2020-09-08 16:55:25 -0400

    Day 12 : Working with XML elements attributes and save it into the List

--- src/test/java/day12/OpenMovieDB_XML.java
+++ src/test/java/day12/OpenMovieDB_XML.java
@@ -1,7 +1,12 @@
 package day12;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -36,6 +41,35 @@ public class OpenMovieDB_XML {
                 .body("root.movie.@released", is("31 Mar 2017"))
                 .body("root.movie.@runtime", is("97 min"))
             ;
+    }
+
+    @DisplayName("Getting may movies and extracting attributes to the list")
+    @Test
+    public void testGettingAttributeAsList(){
+
+        // in open movie DB
+        // the query parameter called s = "some movie name"
+        // will return many movies match that names
+        // and we are using that query parameter
+        // http://www.omdbapi.com/?apikey=YourKeyHere&r=xml&s=Superman
+
+        Response response = given()
+                        .log().all()
+                        .baseUri("http://www.omdbapi.com/")
+                        .queryParam("apikey", "be8cd0d1")
+                        .queryParam("r","xml")
+                        .queryParam("s","superman").
+                when()
+                        .get() ;
+
+        XmlPath xp = response.xmlPath();
+        // get the list of movie title
+        // getting list of attribute belong to each elements in xml nodes/elements
+        List<String> supermanMovieList = xp.getList("root.result.@title") ;
+        System.out.println("supermanMovieList = " + supermanMovieList);
+
+
+
     }
 
 
